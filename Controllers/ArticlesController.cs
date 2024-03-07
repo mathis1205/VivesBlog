273169092b394ba469437ff7d4328086bdbaa8a3
mathis1205 <96077034+mathis1205@users.noreply.github.com>
2024-03-07 12:20:38 +0100

    add controls to data

--- Controllers/ArticlesController.cs
+++ Controllers/ArticlesController.cs
@@ -18,5 +18,44 @@ namespace VivesBlog.Ui.Mvc.Controllers
             return View(articles);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+            _vivesBlogDbContext.Articles.Add(article);
+            _vivesBlogDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var article = _vivesBlogDbContext.Articles.FirstOrDefault(p => p.Id == id);
+            if (article == null) return RedirectToAction("Index");
+            return View(article);
+        }
+        [HttpPost]
+        public IActionResult Edit(Article article)
+        {
+            if (string.IsNullOrWhiteSpace(article.Title) || string.IsNullOrWhiteSpace(article.Author)) return RedirectToAction("Index");
+            _vivesBlogDbContext.Articles.Update(article);
+            _vivesBlogDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var person = _vivesBlogDbContext.Articles.FirstOrDefault(p => p.Id == id);
+            if (person == null) return RedirectToAction("Index");
+            _vivesBlogDbContext.Articles.Remove(person);
+            _vivesBlogDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
